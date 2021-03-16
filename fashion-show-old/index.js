195540631141d245f0971fa9c8a3d93969e498da
WhatIsACore <zekixu@gmail.com>
2021-03-16 00:36:18 -0700

    fashion show page styling

--- fashion-show-old/index.js
+++ fashion-show-old/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-'use strict';
-
 let designers = {
   'Christina Tan': {
     image: 'christina.jpg',
-    desc: `Christina Tan is an undergraduate student attending the University of California, Davis, who creates elegant, casual, and effortless designs in women’s wear that focuses on confidence empowering. From sketching, prototyping, sewing, to modeling, she approaches all of them passionately, allowing others to understand her and her designs as bold and unrestricted. She is currently the Chief of Event Coordinator for an organization called Fashion and Design Society, in which she researches and educates professional fashion resources and opportunities for the Davis Community. She has also worked as a costume producer for the Nutcracker performance, where she creates and alternates costumes for children of different ages. After she graduates, she will continue designing and pursuing a career in the fashion industry and develop her own clothing line to the best of her&nbsp;ability. `
+    desc: `Christina Tan is an undergraduate student attending the University of California, Davis, who creates elegant, casual, and effortless designs in women’s wear that focuses on confidence empowering. From sketching, prototyping, sewing, to modeling, she approaches all of them passionately, allowing others to understand her and her designs as bold and unrestricted. She is currently the Chief of Event Coordinator for an organization called Fashion and Design Society, in which she researches and educates professional fashion resources and opportunities for the Davis Community. She has also worked as a costume producer for the Nutcracker performance, where she creates and alternates costumes for children of different ages. After she graduates, she will continue designing and pursuing a career in the fashion industry and develop her own clothing line to the best of her ability. `
   },
   'Ingrid Yeung': {
     image: 'ingrid.jpg',
@@ -14,13 +12,13 @@ let designers = {
   'Madison Moriarty': {
     image: 'madison.jpg',
     desc: `Madison is a fashion designer based in Santa Barbara, California. She is currently
-    pursuing a B.A. at the University of California, Davis, in fashion&nbsp;design.
+    pursuing a B.A. at the University of California, Davis, in fashion design.
     <div class='space'></div>
     Her collection, Sheer Delight, focuses on the women’s sex appeal with elegant details of chiffon and
     tulle. She often creates garments to enhance a woman’s appearance and provoke self-
     confidence. In addition, her work often enhances the over sexualizing of women as she
     firmly believe that women need to re own our sex appeal in order to stop other
-    people from doing it. Her aesthetic is light, simple, and&nbsp;sexy.
+    people from doing it. Her aesthetic is light, simple, and sexy.
     <div class='space'></div>
     To learn more about Madison, visit <a href='madisonmoriartydesigns.squarespace.com'>madisonmoriartydesigns.squarespace.com</a>
     or on Instagram at <a href='https://instagram.com/madisonmoriartydesigns'>madisonmoriartydesigns</a>.`
@@ -31,17 +29,17 @@ let designers = {
   },
   'Qi Chen': {
     image: 'qi.png',
-    desc: `Qi Chen is a designer and scientist who investigates and innovates clothing based on Chinese traditional design elements and modern fashion design for both men and women that are interested in inheritage of traditional arts and&nbsp;properties.
+    desc: `Qi Chen is a designer and scientist who investigates and innovates clothing based on Chinese traditional design elements and modern fashion design for both men and women that are interested in inheritage of traditional arts and properties.
     <div class='space'></div>
-    She is passionate about creating new trends that arise from old fashion to create possibilities for re-illumination of special designs that are worth appreciating. Her background in Neuroscience also inspires herself to add elements from the macro and micro world of nature into her&nbsp;designs.`
+    She is passionate about creating new trends that arise from old fashion to create possibilities for re-illumination of special designs that are worth appreciating. Her background in Neuroscience also inspires herself to add elements from the macro and micro world of nature into her designs.`
   },
   'Siuyan Wong': {
     image: 'siuyan.jpg',
-    desc: `Siuyan Wong is a senior design major student whose primary focus on fashion and interior design at UC DAVIS. She has been interested in fashion since she was a little kid and first got hooked on sewing in her first year of middle school in Hong Kong. Because of her love for fashion, she took courses in fashion merchandising, but what she loved most was designing clothes and making it a real piece to go to fashion&nbsp;shows.`
+    desc: `Siuyan Wong is a senior design major student whose primary focus on fashion and interior design at UC DAVIS. She has been interested in fashion since she was a little kid and first got hooked on sewing in her first year of middle school in Hong Kong. Because of her love for fashion, she took courses in fashion merchandising, but what she loved most was designing clothes and making it a real piece to go to fashion shows.`
   },
   'Vania Sutandi': {
     image: 'vania.jpg',
-    desc: `Vania Sutandi B.A., University of California, Davis, is a designer who creates clothing with environmentally sustainable processes. She has taken technical fashion design classes such as pattern making and draping, experimental fashion design, functional apparel design, and is now working on a capsule signature collection. She also has experience in textile surface design, especially in making patterns and resists. Furthermore, she has experience in design research during an instructor-led project on children’s protective face masks that focuses on adjustability and&nbsp;comfort.`
+    desc: `Vania Sutandi B.A., University of California, Davis, is a designer who creates clothing with environmentally sustainable processes. She has taken technical fashion design classes such as pattern making and draping, experimental fashion design, functional apparel design, and is now working on a capsule signature collection. She also has experience in textile surface design, especially in making patterns and resists. Furthermore, she has experience in design research during an instructor-led project on children’s protective face masks that focuses on adjustability and comfort.`
   },
   'Yuxin Mao': {
     image: 'yuxin.jpg',
@@ -49,7 +47,7 @@ let designers = {
   },
   'Zhaoran Li': {
     image: 'zhaoran.jpg',
-    desc: `Zhaoran Li is a fashion designer and also a B.A. from the University of California, Davis. She investigates and innovates kids’ apparel based on interesting fashion elements by incorporating unique manipulations with comfort. She is passionate about creating new ideations and aspects from all kinds of elements from both the real world and digital world that can provide her unique inspirations for her designs. Her textile design background also offers her unique and fun fabric pattern designs that can incorporate into her fashion design, such as silkscreen print, Mimaki print, natural dye, and solar fast dye&nbsp;techniques.`
+    desc: `Zhaoran Li is a fashion designer and also a B.A. from the University of California, Davis. She investigates and innovates kids’ apparel based on interesting fashion elements by incorporating unique manipulations with comfort. She is passionate about creating new ideations and aspects from all kinds of elements from both the real world and digital world that can provide her unique inspirations for her designs. Her textile design background also offers her unique and fun fabric pattern designs that can incorporate into her fashion design, such as silkscreen print, Mimaki print, natural dye, and solar fast dye techniques.`
   }
 }
 
