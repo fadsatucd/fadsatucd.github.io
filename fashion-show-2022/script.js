66ed6e1efd32657cd9e6c1d29b70a6ef6f073275
Julia Ma <julma@ucdavis.edu>
2022-03-27 13:36:19 -0700

    -

--- fashion-show-2022/script.js
+++ fashion-show-2022/script.js
@@ -1,12 +1,29 @@
 (function() {
     'use strict';
 
-    const vid = document.getElementById('theme-reveal');
+    /* const vid = document.getElementById('theme-reveal');
     console.log(vid);
 
     vid.addEventListener('ended', function() {
         vid.style.display = "none";
         console.log("vid show go byby");
-    })
+    }) */
+
+    var faq = document.getElementsByClassName("faq-page");
+    var i;
+    for (i = 0; i < faq.length; i++) {
+        faq[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+            /* Toggle between hiding and showing the active panel */
+            var body = this.nextElementSibling;
+            if (body.style.display === "block") {
+                body.style.display = "none";
+            } else {
+                body.style.display = "block";
+            }
+        });
+    }
 
 })();
